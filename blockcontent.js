49f909512c4caf1cd6afe20fcf1a1e29b8c59b3a
agoda <jagadheeswaran.mohan@gmail.com>
2018-12-25 20:06:52 +0530

    i

--- blockcontent.js
+++ blockcontent.js
@@ -18,7 +18,7 @@ var setContentToBuilder = function () {
 }
 
 var addAssetToContentBlock = function () {
-    sdk.setContent(selectedImagePath);
+    sdk.setContent('<img src="'+selectedImagePath+'"/>');
 }
 
 
